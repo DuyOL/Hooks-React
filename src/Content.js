392e6ed6f988a8226a19a65c277f02d8a79beae2
DuyOL <duyskt228@gmail.com>
2023-05-07 00:50:17 +0700

    07-05

--- src/Content.js
+++ src/Content.js
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
+// 1.useEffect(CallBack)
+// - Gọi CallBack mỗi khi Component re-render
+// - Gọi CallBack sau khi Component thêm Element vào DOM
+// 2 useEffect(callback,[])
+// - Chỉ gọi CallBack 1 lần sau khi Componet mounted
 function Content() {
+    const [title, setTitle] = useState('')
+    const [posts, setPosts] = useState([])
+    //CallBack luôn được gọi sau khi Commopent mounted
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/photos')
+            .then(res => res.json())
+            .then(posts => {
+                setPosts(posts);
+            })
+    }, [])
     return (
-        <div style={{ padding: 20 }}>
-            <h1>James Dev Master</h1>
+        <div>
+            <input
+                value={title}
+                onChange={e => setTitle(e.target.value)}
+            />
+            <ul>
+                {posts.map(post => (
+                    <li key={post.id}>{post.id}<br></br>{post.title}</li>
+                ))}
+            </ul>
         </div>
     )
 }
